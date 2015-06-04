8e7e80e9341d6eae9bf8f353cf6401c9d1d1215f
kenshinhu <hujianxincom@gmail.com>
2015-06-04 14:21:26 +0800

    修改刷新数量

--- request.js
+++ request.js
@@ -69,6 +69,10 @@ function newsRequest(n,fn){
                             //.set('Cookies','B2uw_21bb_lastvisit=1433140980; B2uw_21bb_visitedfid=35; _gat=1; B2uw_21bb_auth=0ffe3wD3rSjkCukp%2F7oWxGAJDiEYuwX6dPuw%2Beeyrq3h0X53RLDuKdMWMnWnTrjZt4To0XB6u4nVt9wFfjv4Syeh%2FQ; B2uw_21bb_oldtopics=D1596815D1596804D; B2uw_21bb_fid35=1433144765; _oc_session=RC9paGhzL1RZOUlmQ2hNL1NrZ0pPVm1sRWVlSUtUWUVubEwrQmsrZ3ZIcUFpL28zRDBMUnhYYXFsVzNibGJmd2MxcVYzYW04S002T2hIeEh0NjB0czlaSzVxTWhTZ3MxRnFObTJlNDVMc3ZWOTRRZTlseEF1RkVoSnhKS29Tak9vMGV5amsrdzZ0NU1BQ0ZLQVNZOW82ZC9ZSi9hbm85TnV3eG1BUGFuc0hmcVNHSk1TaU1lV0V3eU9YNHFUVk1uUVBMUVRkL2tRT0l2NTRWMDBiK1dIbFJBcHdVUERjSXNpeTdrL0gwZFZHUVpwMVhqOXB2aXNXRW8wdEtoZ2VmVUoxbUl2MlpKaW5HWU1BOExna3VEczRpYVhTTW9GeWR3d2dtM1IzZVh5M0lHaE43UVEyWjJiUnR6c2lnV3FNZXBDV0lyWVJFZFVmN21pK0R0R01TTXRHYzFzaDBYanVqZlRHSEREcmtTU01uSFFDUU1ydW90OVp1SzNjRC9pWnJNbGJ3RHdGUnpMSTFLcm5RK29oZitnSERKM0R2K1Bna2RoMU9VR1liUFNjRjFFMzRnR0Zha0pwZUx5RTVubzZtRmRjVG01cWwrekdPeXVvNUEwVUVESjU2UzRzdGZ1ZzFVNk9MODIxMHRiZTVZOE5TYWtYQkpTcGlaQllmNyszd1BydUo1TlprVFZoUGszaElxRFlVa1V4V1hiMjFxOEdnQzQ4NlR5WjR5bGFHZXVackNndVZzY1crMTU3cUh6RmdGLS1hV0p2N2VqRzBuNnhTZmk0SHVzNjFnPT0%3D--2229beaddfa1dba078d0fb47f6055434279609e3; B2uw_21bb_sid=xH64I3; B2uw_21bb_lastact=1433144993%09home.php%09spacecp; B2uw_21bb_checkpm=1; B2uw_21bb_smile=4D1; _ga=GA1.2.1821348410.1433144582')
                             .end(function(err,res){
 
+                                if(res==null){
+                                    return next();
+                                }
+
                                 var _ = cheerio.load(res.text,{
                                     normalizeWhitespace: true,
                                     xmlMode: true,
